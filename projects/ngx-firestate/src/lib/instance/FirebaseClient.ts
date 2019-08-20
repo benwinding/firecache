1c8a47966f8292edbdcf541832f5a5f053e755fc
Ben Winding <ben.winding@gmail.com>
2019-08-20 17:45:10 +0930

    Deploy change

--- projects/ngx-firestate/src/lib/instance/FirebaseClient.ts
+++ projects/ngx-firestate/src/lib/instance/FirebaseClient.ts
@@ -2,6 +2,8 @@ import { FirebaseWrapper } from './firebase/FirebaseWrapper';
 import { FirebaseClientState } from './FirebaseClientState';
 import { FirestoreWrapper } from './firebase/provider/FirestoreWrapper';
 import { FirebaseConfigObject } from './firebase/provider/firebase-helpers';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export class FirebaseClient<EnumDocType> {
   private firebaseWrapper: FirebaseWrapper<EnumDocType>;
@@ -27,6 +29,14 @@ export class FirebaseClient<EnumDocType> {
     } catch (error) {
       console.error('FirebaseClient.ts: InitFirebase()', error);
     }
+  }
+
+  public $IsLoggedIn(): Observable<boolean> {
+    return this.clientState.$user.pipe(map(u => !!u));
+  }
+
+  public $CurrentUser(): Observable<firebase.auth.UserCredential> {
+    return this.clientState.$user;
   }
 
   public login(email: string, pass: string) {
