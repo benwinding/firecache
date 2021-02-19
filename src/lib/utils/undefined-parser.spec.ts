417bfb897d0b10da5d8235b11cf9c5912f3c5a9e
Ben Winding <hello@benwinding.com>
2021-02-19 12:26:20 +1030

    Fix for removing undefined values

--- src/lib/utils/undefined-parser.spec.ts
+++ src/lib/utils/undefined-parser.spec.ts
@@ -30,6 +30,31 @@ describe("timestamp-parser tests", () => {
     const parsed = getWithoutUndefined(doc);
     expect(parsed).toMatchObject({});
   });
+
+  test("converts undefined from array", () => {
+    const doc = { a: [ undefined ] };
+    const parsed = getWithoutUndefined(doc);
+    expect(parsed).toMatchObject({a: []});
+  });
+
+  test("converts complex object", () => {
+    const doc = { a: [ undefined, { b: 0, c: '', d: undefined } ] };
+    const parsed = getWithoutUndefined(doc);
+    expect(parsed).toMatchObject({a: [{b: 0,c: ''}]});
+  });
+
+  test("converts circular referencing objects", () => {
+    const a = {
+      array: [undefined]
+    }
+    const b = {
+      c: 'A string',
+      a: a
+    }
+    a.array.push(b);
+    const doc = { a: a };
+    expect(() => getWithoutUndefined(doc)).not.toThrow();
+  });
 });
 
 function makeTimestamp() {
--- src/lib/utils/undefined-parser.ts
+++ src/lib/utils/undefined-parser.ts
@@ -1,8 +1,45 @@
 export function getWithoutUndefined<T>(obj: T) {
   try {
-    const objParsed = JSON.parse(JSON.stringify(obj));
-    return objParsed;
+    recusivelyCheckObjectValueForUndefined(obj, 0);
+    return obj;
   } catch (error) {
     console.error(error, { obj });
+  }
+}
+
+const RECURSION_LIMIT = 15;
+
+function recusivelyCheckObjectValueForUndefined(obj: any, level: number): any {
+  if (level >= RECURSION_LIMIT) {
+    return;
+  }
+  if (!obj) {
+    return;
+  }
+  if (Array.isArray(obj)) {
+    const arr = obj as [];
+    const indexesToRemove = [];
+    arr.map((val, index) => {
+      if (val === undefined) {
+        indexesToRemove.push(index);
+      } else {
+        recusivelyCheckObjectValueForUndefined(val, level+1);
+      }
+    })
+    indexesToRemove.map(i => arr.splice(i, 1));
+    return;
+  }
+  const isObject = typeof obj === 'object';
+  if (isObject) {
+    const keysToRemove = [];
+    Object.entries(obj).map(([key, value]) => {
+      if (value === undefined) {
+        keysToRemove.push(key);
+      } else {
+        recusivelyCheckObjectValueForUndefined(value, level+1);
+      }
+    })
+    keysToRemove.map(k => delete obj[k]);
+    return;
   }
 }
